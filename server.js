a4e057fdf1d07a39134f0e4af8eabedbd871713d
ti-metiss <coutenay.yoan@hotmail.fr>
2023-02-14 18:48:00 +0100

    connexion BDD

--- server.js
+++ server.js
@@ -1,6 +1,28 @@
 const app = require("./app");
+const dotenv = require("dotenv");
+const mongoose = require("mongoose");
+
+dotenv.config({ path: "./config.env" });
+
+const DB = process.env.DATABASE.replace(
+  "<password>",
+  process.env.DATABASE_PASSWORD
+);
+
+mongoose.set("strictQuery", true);
+mongoose
+  .connect(DB, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+  .then((data) => {
+    console.log("connection established");
+  })
+  .catch((e) => {
+    console.log(`error ${e}`);
+  });
 
 const port = process.env.PORT || 3000;
 const server = app.listen(port, () => {
-  console.log("test server listening on port");
+  console.log(`Server listening on port ${port}`);
 });
