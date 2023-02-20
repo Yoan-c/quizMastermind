12357f29873bc15b2662f44d2945eb545f57772e
ti-metiss <coutenay.yoan@hotmail.fr>
2023-02-20 06:20:24 +0100

    front form

--- controllers/viewController.js
+++ controllers/viewController.js
@@ -1,10 +1,28 @@
-const AppError = require('../utils/appError');
-const catchAsync = require('../utils/catchAsync');
-
+const AppError = require("../utils/appError");
+const catchAsync = require("../utils/catchAsync");
 
 exports.index = (req, res, next) => {
-    res.render(`${__dirname}/../view/pages/index`, {
-        title : "Quiz Mastermind"
-    });
+  // RECHERCHER LES QUIZ ET LEUR DESCRIPTION + IMG
+  res.render(`${__dirname}/../view/pages/index`, {
+    title: "Quiz Mastermind",
+  });
+};
 
-}
+exports.getFormLogin = (req, res, next) => {
+  res.render(`${__dirname}/../view/pages/index`, {
+    title: "Quiz Mastermind",
+    askConnect: "login",
+  });
+};
+exports.getFormSignUp = (req, res, next) => {
+  res.render(`${__dirname}/../view/pages/index`, {
+    title: "Quiz Mastermind",
+    askConnect: "sign up",
+  });
+};
+
+exports.login = (req, res, next) => {
+  res.render(`${__dirname}/../view/pages/index`, {
+    title: "Quiz Mastermind",
+  });
+};
--- routes/viewRoute.js
+++ routes/viewRoute.js
@@ -1,7 +1,12 @@
-const express = require('express')
-const viewController = require('../controllers/viewController')
+const express = require("express");
+const viewController = require("../controllers/viewController");
 const router = express.Router();
 
-router.route('/').get(viewController.index)
+router.route("/").get(viewController.index);
+router
+  .route("/login")
+  .get(viewController.getFormLogin)
+  .post(viewController.login);
+router.route("/signup").get(viewController.getFormSignUp);
 
-module.exports = router
+module.exports = router;
